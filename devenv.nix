719a6f8de7349ad0293481013b9154fc6deb3f19
Marcus Ramberg <marcus@means.no>
2023-05-18 16:58:29 +0200

    Add content/post/2023-05-18-manage-hugo-with-devenv.md

--- devenv.nix
+++ devenv.nix
@@ -1,37 +1,31 @@
 { pkgs, ... }:
 
 {
-  # https://devenv.sh/basics/
   env.LANG = "en_US.UTF-8";
 
-  # https://devenv.sh/packages/
   packages = with pkgs; [
     hunspellDicts.en_US
     hugo
   ];
 
-  # https://devenv.sh/scripts/
-  scripts.new.exec = ''
+  scripts.mkpost.exec = ''
     POST=content/post/$(date +%Y-%m-%d)-$1.md
     hugo new $POST
     nvim $POST
     git add $POST
     git commit -am"Add $POST"
   '';
+
   processes = {
-    hugo = "hugo serve";
+    hugo.exec = "hugo serve";
   };
 
-  enterShell = ''
-    hugo
-  '';
+  enterShell = "hugo";
 
   # https://devenv.sh/pre-commit-hooks/
-  pre-commit.hooks.markdownlint.enable = true;
-  pre-commit.hooks.hunspell.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
+  pre-commit.hooks = {
+    actionlint.enable = true;
+    hunspell.enable = true;
+    markdownlint.enable = true;
+  };
 }
