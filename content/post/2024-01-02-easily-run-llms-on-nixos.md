1b13f524f893dd25d537288e0d23351e37c38243
Marcus Ramberg <marcus@means.no>
2024-01-02 18:13:23 +0100

    feat: Add a post about running LLMs on nixos

---
+++ content/post/2024-01-02-easily-run-llms-on-nixos.md
@@ -0,0 +1,28 @@
++++
+title = "Easily Run LLMs on Nixos"
+date = "2024-01-02"
+tags = [ "post", "llm", "nixos" ]
+categories = ["nixos"]
+
+comments = true
+published = true
++++
+
+With the recent AI craze, it's fun to play with local models. I like [ollama](https://ollama.ai/),
+which lets you easily download and interact with various models. Thanks to this
+recently merged [PR](https://github.com/NixOS/nixpkgs/pull/277442), it's now trivial to run your own ollama
+service on nixos:
+
+```nix
+services.ollama.enable = true;
+```
+
+Adding this to your host config and rebuilding is all it takes to start the service,
+and you can then interact with it via the cli or rest interface. For instance to talk
+to the [mistral](https://mistral.ai/product/) model, you can do:
+
+<img src="/images/2024-01-02-mistral.png"  width="720" alt="ollama run mistral"/>
+
+On first run it will automatically download the model, and then you can interact with it.
+They support a [bunch of models](https://ollama.ai/library/), so have fun exploring! If you
+are of that inclination, there's also a [neovim plugin](https://github.com/nomnivore/ollama.nvim).
--- devenv.nix
+++ devenv.nix
@@ -8,15 +8,28 @@
     hunspellDicts.en_US
     hugo
     nodePackages.markdownlint-cli
-  ];
+  ] ++ lib.optional stdenv.isLinux xclip ++ lib.optional stdenv.isDarwin pngpaste;
 
-  scripts.mkpost.exec = ''
-    POST=content/post/$(date +%Y-%m-%d)-$1.md
-    hugo new $POST
-    nvim $POST
-    git add $POST
-    git commit -am"Add $POST"
-  '';
+  scripts = {
+    mkpost.exec = ''
+      POST=content/post/$(date +%Y-%m-%d)-$1.md
+      hugo new $POST
+      nvim $POST
+      git add $POST
+      git commit -am"Add $POST"
+    '';
+    pasteimg.exec = ''
+      FILE=static/images/$(date +%Y-%m-%d)-$1.png
+      if [ -x "$(command -v pngpaste)" ]; then
+        pngpaste $FILE
+        echo "<img src=\"/images/$(basename $FILE)\" />" | pbcopy
+      else
+        xclip -selection clipboard -t image/png -o > $FILE
+        echo "<img src=\"/images/$(basename $FILE)\" />" | xclip -selection clipboard
+      fi
+    '';
+
+  };
 
   processes = {
     hugo.exec = "hugo serve";
@@ -27,8 +40,8 @@
   # https://devenv.sh/pre-commit-hooks/
   pre-commit.hooks = {
     actionlint.enable = true;
-    hunspell.enable = true;
-    hunspell.files = lib.mkForce "\\.md$";
+    # hunspell.enable = true;
+    # hunspell.files = lib.mkForce "\\.md$";
     markdownlint.enable = true;
   };
 }
